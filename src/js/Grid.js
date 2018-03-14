5292f3ed9c07286d946a2183faf9e4f1d69b589b
Bartosz Mediger <bartosz.mediger@withpassion.pl>
2018-03-14 11:43:54 +0100

    AtvImg and some fixes.

--- src/js/Grid.js
+++ src/js/Grid.js
@@ -41,49 +41,52 @@ export function grid() {
             document.addEventListener('lazybeforeunveil', function() {
                 macyInstance[i].recalculate(true);
             });
-
-            console.log('f');
             
             macyInstance[i].reInit();
         }        
+        
+        console.log('macy init');
     }
     
     
     const destroy = function() {
     
-        ww = window.innerWidth;
         
-        if ((ww <= 768) && (mobile === false)) {
         
-
-                for (let i = 0; i < grid.length; i ++) {
-                    if (macyInstance[i] != undefined) {
-                        macyInstance[i].remove();
+        setTimeout(function() {
+            ww = window.innerWidth;
+            
+            if (ww <= 768) {
+                if (mobile === false) {
+    
+                    for (let i = 0; i < grid.length; i ++) {
+                        if (macyInstance[i] != undefined) {
+                            macyInstance[i].remove();
+                        }
                     }
+                    
+                    mobile = true;
+                    console.log('destroy');
                 }
-                
-                mobile = true;
+            }
             
-                console.log('destroy');
-
-
-            
-        }
+            if (ww > 768) {
+                if (mobile === true) {
+                    init();
+                    mobile = false;
+                }
+            }
+        }, 200);
         
-        if ((ww > 768) && (mobile === true)) {
         
-
-                console.log('init');
-                init();
-                mobile = false;
-
-        }
     }
     
     window.addEventListener('resize', destroy);
     
     if (ww > 768) {
-        init();            
+        if (mobile === false) {
+            init();
+        }         
     }
     
     
